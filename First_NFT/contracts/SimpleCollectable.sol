4727b58d020103c2ea3ca86ab424f57243dbada3
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-05-16 21:31:45 +0600

    import file

--- First_NFT/contracts/SimpleCollectable.sol
+++ First_NFT/contracts/SimpleCollectable.sol
@@ -1,3 +1,11 @@
-// SPDX-Lincence-Identifier: MIT
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidtiy ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectable is ERC721 {
+
+    constructor () public ERC721("cutie", "CUTU"){
+        
+    }
+}
