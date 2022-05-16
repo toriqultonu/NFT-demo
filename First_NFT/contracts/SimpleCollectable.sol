1cd8b3b0a2cb187b451c089beb19eeca694b9b54
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-05-16 21:57:24 +0600

    createCollectable added

--- First_NFT/contracts/SimpleCollectable.sol
+++ First_NFT/contracts/SimpleCollectable.sol
@@ -4,8 +4,17 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectable is ERC721 {
+    uint256 public tokenCounter;
 
-    constructor () public ERC721("cutie", "CUTU"){
-        
+    constructor() public ERC721("cutie", "CUTU") {
+        tokenCounter = 0;
+    }
+
+    function createCollectable() public returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        tokenCounter = tokenCounter + 1;
+
+        return newTokenId;
     }
 }
